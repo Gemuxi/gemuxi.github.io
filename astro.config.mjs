53c87921dd1df5acb7ebe192df3313f0dba2687b
Molasses <2018063+sleeplessai@users.noreply.github.com>
2026-01-01 03:02:15 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://gemuxi.github.io/fantastic-meme/",
-	base: "/",
+	site: "https://gemuxi.github.io/",
+	base: "/fantastic-meme",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
